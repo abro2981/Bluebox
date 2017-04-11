a7bd5b5bf58f48e6028f2ea86ad6cda8ec5bef6b
Tommy Ha <tha@csumb.edu>
2017-04-11 21:14:15 +0000

    completed rubric # 2 and 3

--- bluebox.php
+++ bluebox.php
@@ -9,15 +9,60 @@ $conn = getDBConnection("bluebox");
 //filter by genre
 
 function getGenre($genreID){
-    $sql = "SELECT * FROM genres NATURAL JOIN movies NATURAL JOIN prices WHERE genreId = 1 ";
+    global $conn;
+    $NameParam = array();
+    $NameParam[":genreId"] = $genreID;
+    $sql = "SELECT * FROM genres NATURAL JOIN movies NATURAL JOIN prices WHERE genreId = :genreId ";
+    $stmt = $conn -> prepare ($sql);
+    $stmt -> execute($NameParam);
+    $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    return $records;
 }
 
 //filter by title 
 
+function getTitle($title){
+    global $conn;
+    $NameParam = array();
+    $NameParam[":title"] = '%'.$title.'%';
+    $sql = "SELECT movieName, mediaType, priceValue FROM genres NATURAL JOIN movies NATURAL JOIN prices WHERE movieName LIKE :title";
+    $stmt = $conn -> prepare ($sql);
+    $stmt -> execute($NameParam);
+    $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+    return $records;
+}
 
 
 //filter by price
+
+function getPrice(){
+    global $conn;
+    $sql = "SELECT movieName, mediaType, priceValue FROM genres NATURAL JOIN movies NATURAL JOIN prices ORDER BY priceValue ASC";
+    $stmt = $conn -> prepare ($sql);
+    $stmt -> execute();
+    $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    return $records;
+}
+
+//sort by either price, title, or genre
+//parameters ex. --> (price, asc)
+//param arg --> movieName, genreID, or priceValue
+//asc arg --> asc or desc
+function sortData($param, $sort){
+    global $conn;
+    $NameParam = array();
+    $NameParam[":param"] = $param;
+    $NameParam[":sort"] = $sort;
+    $sql = "SELECT movieName, mediaType, priceValue FROM genres NATURAL JOIN movies NATURAL JOIN prices ORDER BY :param :sort";
+    $stmt = $conn -> prepare ($sql);
+    $stmt -> execute($NameParam);
+    $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    return $records;
+}
 
 
 ?>
