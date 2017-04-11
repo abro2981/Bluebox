8549d57b88c4293590ea6c32f8b0faaf28b17dde
Tommy Ha <tha@csumb.edu>
2017-04-11 21:11:12 +0000

    completd rubric requirements 2 and 3

--- bluebox.php
+++ bluebox.php
@@ -6,7 +6,18 @@ include '../../inc/dbConnection.php';
 $conn = getDBConnection("bluebox");
 
 
-//
+//filter by genre
+
+function getGenre($genreID){
+    $sql = "SELECT * FROM genres NATURAL JOIN movies NATURAL JOIN prices WHERE genreId = 1 ";
+}
+
+//filter by title 
+
+
+
+
+//filter by price
 
 
 ?>
