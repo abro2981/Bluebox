e39b6ec5628a31ce8cfa0edaba350f13723c4605
bonedaddy4538 <jsainz@csumb.edu>
2017-04-13 21:18:10 +0000

    sike i forgot one page should be final layout

--- movieInfo.php
+++ movieInfo.php
@@ -56,7 +56,8 @@ $title = getTitle($_GET["movieId"]);
     <body class="wrapper">
         <div class="jumbotron" style = "margin-left:auto;margin-right:auto">
             <h1>Movie Info</h1>
-            <nav><a href = "index.php" id = "currentPage">Home Page</a><br/><a href = "cart.php">Shopping Cart</a></nav>
+            <nav class="navbar navbar-dark bg-primary"><a style = "color:black;" href = "index.php" id = "currentPage">Home Page</a><br/><a 
+            style = "color:black" href = "cart.php">Shopping Cart</a></nav>
         </div>
         <?php
         
